4e62d89d942414e9ada18a514a64d04d1539f89b
NonBlu <sallowsaiden@gmail.com>
2023-07-31 13:52:40 -0700

    Display function
    Displays board given a single array

--- CS4A_Queens_Project.java
+++ CS4A_Queens_Project.java
@@ -1,7 +1,30 @@
 public class CS4A_Queens_Project {
+
+    public static void display(int[] chess) //Created by Aiden Sallows
+    {
+        for(int i = 0; i < chess.length; i++)
+        {
+            if(chess[i] == 0)
+            {
+                System.out.print("| ");
+            }
+            else if(chess[i] == 1)
+            {
+                System.out.print("|Q");
+            }
+
+            if((i + 1) % 8 == 0)
+            {
+                System.out.print("|\n");
+            }
+        }
+    }
     public static void main(String[] arg) {
 
-        System.out.println("Hello World");
-        System.out.println("Hello again");
+        int[] chess = new int [64];
+        chess[34] = 1;
+        chess[16] = 1;
+
+        display(chess);
     }
 }
